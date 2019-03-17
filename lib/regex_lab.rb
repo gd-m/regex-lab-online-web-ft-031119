5a5061a56a8c0afda1eef6e985d222e3479ce4a7
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-17 21:05:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,13 @@
 def starts_with_a_vowel?(word)
   word.match(/\A[aeiouAEIOU]/) != nil
 
+#one more way to do it
+  #if word.match(/^[AEIOU]|^[aeiou]/)
+  #  true
+  #else
+  #  false
+  #end
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
